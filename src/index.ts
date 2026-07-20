2f270b057c5b6047db514fc720b7103e1aecef5e
Lukas Knoch-Girstmair <lukas.knoch-girstmair@bitmovin.com>
2026-07-20 15:43:45 +0200

    Reset Google DAI companion split base

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './googleDai';
-export * from './googleDaiSourceConfig';
--- typings/bitmovin-player-react-native.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'bitmovin-player-react-native' {
-  export class Player {
-    readonly nativeId: string;
-    isInitialized: boolean;
-    isDestroyed: boolean;
-    initialize(): Promise<void>;
-    destroy(): void;
-  }
-}
